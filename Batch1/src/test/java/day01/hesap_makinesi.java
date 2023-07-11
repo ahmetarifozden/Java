59c42419ad5879bf8f0b22c28cf0a558da01bc5f
ahmetarifozden <116322188+ahmetarifozden@users.noreply.github.com>
2023-07-11 16:19:07 +0300

    Update hesap_makinesi code
    yorum satırı eklendi

---
+++ Batch1/src/test/java/day01/hesap_makinesi.java
@@ -0,0 +1,20 @@
+package day01;
+
+import java.util.Scanner;
+
+// Ahmet Aarif Özden
+
+public class day1 {
+
+    public static void selamlama(){
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        selamlama();
+
+    }
+}
